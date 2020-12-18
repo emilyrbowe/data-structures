b02bdd566cbce438f828e605fbdcd4b2ff073761
Emily Bowe <emilyrbowe@gmail.com>
2020-12-18 13:48:23 -0500

    Process blog script fix

--- final_app/public/js/pb.js
+++ final_app/public/js/pb.js
@@ -3,53 +3,48 @@
 var writingType;
 
 $(document).ready(function() {
-    $(".btn").on('click', function(e) {
-        
-        $(this).addClass("active");
-        $(this).siblings().removeClass('active')
-        
-        if (this.classList.contains('active')){
-            writingType = $(this).attr("id");
-        }
-        showTexts(writingType)
-    });
-  
-  //show texts in a sidebar
-  $(".writing").on('click', function(e) {
+  $(".btn").on('click', function(e) {
+
     $(this).addClass("active");
     $(this).siblings().removeClass('active')
-    
-    if (this.classList.contains('active')){
-        var writingToShow = $(this).html();
-        $('#viewer').html(`${writingToShow}`);
+
+    if (this.classList.contains('active')) {
+      writingType = $(this).attr("id");
     }
-  showTexts(writingType)
+    showTexts(writingType)
   });
 });
 
+function showSidebar(el) {
+  $(el).addClass("selected");
+  $(el).siblings().removeClass('selected')
+  let sidebarContent = ($(el).html())
+  $('#viewer').html(`${sidebarContent}`);
+};
 
-function showTexts(writingType){
+function showTexts(writingType) {
   var parameters = {
     thesisWriting: writingType,
   };
-  
+
   console.log(parameters);
-  
+
   //getting server endpoint
-  $.get('/processblog', parameters, function(data){
+  $.get('/processblog', parameters, function(data) {
     console.log(data);
-    let content ='';
+    let content = '';
     // console.log(data.pbtext);
-    data = data.pbtext;
-    for (var i=0; i<data.length; i++) {
-        content += `
-        <div class="writing ${data[i].PK_category.S}">
-            ${data[i].entry.S}
+    // data = data.pbtext;
+    for (var i = 0; i < data.pbtext.length; i++) {
+      content +=
+        `
+        <div class="writing" onclick="showSidebar(this)">
+            ${data.pbtext[i].entry.S}
         </div>
         `
-        };
+    };
     // console.log(content);
     $('#myWriting').html(`${content}`);
-    $('#viewer').html();
-    })
+    $('#viewer').html('Click on one of the documents to preview it here');
+  })
 };
